bdf190ac7456f267c2300c1da67e2c712eb09ec4
Jesper Danielsson <jesperdanielsson1@gmail.com>
2023-01-21 11:54:55 +0100

    fix: Update the project to C++ unreal version 5.1

--- IndieProject/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "C:/Forsbergsskola/Unreal/IndieProject/IndieProject/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
---
+++ Source/Soloproject/SoloprojectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SoloprojectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASoloprojectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASoloprojectGameMode();
+};
+
+
+
